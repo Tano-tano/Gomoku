16c166a58707f45d1486565f101e7fe8d1c96600
Ryota Itano <e1703@s.akashi.ac.jp>
2021-11-06 04:14:11 +0900

    Add files via upload

--- main.c
+++ main.c
@@ -3,36 +3,34 @@
 #include <time.h>
 
 int isOn(int, int);
+int faul(int, int);
 
 int main(){
     FILE* fp;
     char* name = "field.csv";
 
-    int cnt = 0;
+    int cnt = 0;//文字数
     int charcnt = 0;
     int turn = 0;
-    char field[256];
+    char* field = "";
+    char buf[1000];
+
 
     int player = 1;
     srand((unsigned int)time(NULL));
+
+    //ファイルがあるか確認
+    fp = fopen(name, "r");
+    if(fp == NULL){
+        printf("no file");
+    }
+
+    //====================================後は任せた()
+    //碁石を置く座標
     int x = rand()%15;
     int y = rand()%15;
 
-    //ファイルがあるか確認
-    fp = fopen(name, "w");
-    if(fp == NULL){
-        fprintf(fp, "%d", 0);
-    }
-    fclose(fp);
-
-
-    fp = fopen(name, "r");
-    //手番の更新
-    rewind(fp);
-    turn = fgetc(fp);
-    fseek(fp, 0L, SEEK_END);
-
-    //ファイルの長さから手番を確認
+    //ファイルの長さからプログラム番号を確認
     while(1){	
         charcnt = fgetc(fp) ;
         if (charcnt == EOF){
@@ -40,34 +38,56 @@ int main(){
         }
         cnt ++ ;
     }
-    fclose(fp);
 
-    //これまでの結果をバッファに記録
-    fp = fopen(name, "r");
-    fseek(fp, 1L, SEEK_SET);
-    fgets(field, 256, fp);
-    fclose(fp);
-
-    //プログラム番号を設定(ごり押し)
+    //プログラム番号を設定
     if((cnt/6) % 2 != 0){
         player = 2; //1,1:1:1 → 7文字なので 6で割ったら奇数
     }else{
         player = 1; //2,1:1:1,2:2:2 → 13文字なので6で割ったら偶数
     }
 
+    //碁石があるかの確認
+    isOn(x, y);
+
+    //禁手確認
+    if(player == 1) faul(x, y);
+
+
+    //====================================
+
+
+    //ターン数の設定
+    if(cnt == 0){
+        turn = 0;
+    }else{
+        fseek(fp, 0, SEEK_SET);
+        turn = fgetc(fp) - '0' + 1;
+        printf("%d/n", turn);
+        //現在の盤面を保存
+        fseek(fp, 1, SEEK_SET);
+        field = fgets(buf, 1000, fp);
+    }
+
+    //ターン数の書き込み
     fp = fopen(name, "w");
     fprintf(fp, "%d", turn);
     fclose(fp);
-
+    
+    //配置場所の書き込み
     fp = fopen(name, "a");
     fprintf(fp, "%s", field);
-
-    fprintf(fp, "%d:%d:%d", player, y, x); // (プログラム番号:縦:横)
+    fprintf(fp, ",%d:%d:%d", player, y, x); // (プログラム番号:縦:横)
+    fclose(fp);
 
     return 0;
 }
 
 //石が置いてあるかどうか
 int isOn(int x, int y){
+    return 0;
+}
+
+//禁手判定
+int faul(int x, int y){
     return 0;
 }
